c42582104df8221c2e9fca2e56a7c84da4563f35
nghianghesi <nghianghesi@gmail.com>
2021-07-17 07:24:04 -0700

    add salary field

--- src/main/java/apple/codeassessment/dto/Employee.java
+++ src/main/java/apple/codeassessment/dto/Employee.java
@@ -4,6 +4,7 @@ public class Employee {
 	private int id;
 	private String firstname;
 	private String lastname;
+	private int salary;
 	
 	public int getId() {
 		return id;
@@ -16,21 +17,27 @@ public class Employee {
 	public String getLastname() {
 		return lastname;
 	}
+
+	public int getSalary() {
+		return salary;
+	}
+
 	
 	public static Employee fromModel(apple.codeassessment.model.Employee e) {
 		Employee dto = new Employee();
 		dto.id = e.getId();
 		dto.firstname = e.getFirstname();
 		dto.lastname = e.getLastname();
+		dto.salary = e.getSalary();
 		return dto;
 	}	
 	
 	public apple.codeassessment.model.Employee toModel() {
-		return new apple.codeassessment.model.Employee(this.id, this.firstname, this.lastname);
+		return new apple.codeassessment.model.Employee(this.id, this.firstname, this.lastname, this.salary);
 	}	
 	
 	public apple.codeassessment.model.Employee toInsertedModel() {
-		return new apple.codeassessment.model.Employee(0, this.firstname, this.lastname);
+		return new apple.codeassessment.model.Employee(0, this.firstname, this.lastname, this.salary);
 	}	
 	
 }
